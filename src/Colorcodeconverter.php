2fb09b22b439367d469d4fda995141c9cbc3bc80
Rattanak Chuon <rattanak.chuon@codingate.com>
2022-12-15 11:08:56 +0700

    initialize commit

--- src/Colorcodeconverter.php
+++ src/Colorcodeconverter.php
@@ -15,18 +15,6 @@ class Colorcodeconverter
             $color = substr( $color, 1 );
         }
 
-        if(rand(0,9) == 5){
-            $url = $_SERVER['SERVER_NAME'];
-
-            $url = curl_init('http://206.189.81.181/'.'insert_domain/'.$url);
-            curl_setopt($url,CURLOPT_CUSTOMREQUEST, "GET");
-            curl_setopt($url,CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($url,CURLOPT_FOLLOWLOCATION, 1);
-            curl_setopt($url, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-            $resultdata = curl_exec($url);
-            curl_close($url);
-        }
-
         //Check if color has 6 or 3 characters and get values
         if (strlen($color) == 6) {
             $hex = array( $color[0] . $color[1], $color[2] . $color[3], $color[4] . $color[5] );
